031390c07c984cf8a93e3d690e28315b05086fc7
Jackson Pybus <jrpybus@Jacksons-MacBook-Pro.local>
2020-03-18 16:48:39 -0500

    Initial commit, establishing file structures and adding basic files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# GCF_Generator_Suite
+A suite of event generators utilizing the Generalized Contact Formalism to describe interactions with nuclear SRC pairs.
+
+This repository will contain the code and necessary libraries for generating events involving nuclear SRCs interacting in the PWIA framework.
---
+++ src/libraries/nucleus/gcfNucleus.hh
@@ -0,0 +1,90 @@
+#ifndef __NUCLEAR_INFO_H__
+#define __NUCLEAR_INFO_H__
+#include <vector>
+
+enum NNModel {AV18, AV4Pc, N2LO_10, N2LO_12, N3LO_600};
+
+class gcfNucleus
+{
+ public:
+  gcfNucleus(int thisZ, int thisN, char* uType);
+  gcfNucleus(int thisZ, int thisN, NNModel uType);
+  ~gcfNucleus();
+  double get_S(double krel, int l_type, int r_type);
+  double get_pp(double k_rel);
+  double get_nn(double k_rel);
+  double get_pn(double k_rel);
+  double get_pn0(double k_rel);
+  double get_pn1(double k_rel);
+  NNModel get_InteractionType();
+  int get_Z();
+  int get_N();
+  int get_A();
+  double get_mA();
+  double get_mbar();
+  double get_mAmpp();
+  double get_mAmpn();
+  double get_mAmnn();
+  double get_sigmaCM();
+  double get_sigmaE();
+  double get_Estar();
+  double get_Cnn0();
+  double get_Cpp0();
+  double get_Cpn0();
+  double get_Cpn1();
+  void randomize();
+  
+  void set_Nucleus(int thisZ, int thisN);
+  void setCustomValues(double newSigma, double newEstar, double newCpp0, double Cnn0, double newCpn0, double newCpn1);
+  void set_Interaction(NNModel thisNNType);
+  void set_Interaction(char* thisNNType);
+  void set_sigmaCM(double newSigma);
+  void set_Estar(double newEstar);
+  void set_Cpp0(double newCpp0);
+  void set_Cnn0(double newCnn0);
+  void set_Cpn0(double newCpn0);
+  void set_Cpn1(double newCpn1);
+  void set_sigmaE(double newSigE);
+  
+ private:
+  int Z;
+  int N;
+  int A;
+  NNModel u;
+  double mA;
+  double mAmpp;
+  double mAmpn;
+  double mAmnn;
+  double sigmaCM;
+  double d_sigmaCM;
+  double phiSq_pp0[100];
+  double phiSq_nn0[100];
+  double phiSq_pn0[100];
+  double phiSq_pn1[100];
+  double Cpp0;
+  double d_Cpp0;
+  double Cnn0;
+  double d_Cnn0;
+  double Cpn0;
+  double d_Cpn0;
+  double Cpn1;
+  double d_Cpn1;
+  double Estar;
+  double Estar_max;
+  double sigmaE;
+  
+  double get_phiSq(double *phiPtr, double k_rel);
+
+  bool set_Contacts_SS_r();
+  bool set_Contacts_SS_k();
+  bool set_Contacts_EG2();
+  
+  void fill_arrays_AV18();
+  void fill_arrays_n2lo_local();
+  void fill_arrays_n3lo_nonlocal();
+  void fill_arrays_n2lo_12_local();
+  void fill_arrays_AV4Pc();
+  
+};
+
+#endif
