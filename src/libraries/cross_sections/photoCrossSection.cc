d744e3e4352940ec9108e0e1d12b32e80c277230
Jackson Pybus <jrpybus@Jacksons-MacBook-Pro.local>
2020-03-31 17:16:06 -0500

    Added J/Psi cross section and polarization ratio.

--- src/libraries/cross_sections/photoCrossSection.cc
+++ src/libraries/cross_sections/photoCrossSection.cc
@@ -70,3 +70,57 @@ double photoCrossSection::sigma_pim_p(double s, double t)
   return sigma_pip_n(s,t);
 }
 
+double photoCrossSection::sigma_psi_p(double s, double t)
+{
+  double sig0 = 11.3; //nb
+  double beta = 1.3;
+  double Lambda = 1.14;
+  
+  double x = (sq(mpsi) + 2*mN*mpsi)/(s - sq(mN));
+  if ((x > 1.) || (x < 0.))
+    return 0.;
+  
+  double sig = sig0 * pow(1. - x,beta);
+
+  double pi = (sq(mN) - s)/(2.*sqrt(s));
+  double pf = 0.5*sqrt(sq(sq(mN) - sq(mpsi))/s - 2.*(sq(mN) + sq(mpsi)) + s);
+  double tmin = sq(sq(mpsi))/(4.*s) - sq(pi + pf);
+  double tmax = sq(sq(mpsi))/(4.*s) - sq(pi - pf);
+  
+  return sig * dipole_F(t, tmin, tmax);
+}
+
+double photoCrossSection::sigma_psi_p(double s, double t, double QSq)
+{
+  double sig0 = 11.3; //nb
+  double beta = 1.3;
+  double Lambda = 1.14;
+  double n = 2.44;
+  
+  double x = (sq(mpsi) + 2*mN*mpsi)/(s - sq(mN));
+  if ((x > 1.) || (x < 0.))
+    return 0.;
+  
+  double sig = sig0 * pow(1. - x,beta);
+
+  double pi = (sq(mN) - s)/(2.*sqrt(s));
+  double pf = 0.5*sqrt(sq(sq(mN) - sq(mpsi))/s - 2.*(sq(mN) + sq(mpsi)) + s);
+  double tmin = sq(sq(mpsi) + QSq)/(4.*s) - sq(pi + pf);
+  double tmax = sq(sq(mpsi) + QSq)/(4.*s) - sq(pi - pf);
+  
+  return sig * dipole_F(t, tmin, tmax) * pow(sq(mpsi)/(QSq + sq(mpsi)),n);
+}
+
+double photoCrossSection::R_psi_p(double QSq)
+{
+  double a = 2.164;
+  double n = 2.131;
+  return pow((a*sq(mpsi) + QSq)/(a*sq(mpsi)),n) - 1.;
+}
+
+double photoCrossSection::dipole_F(double t, double tmin, double tmax)
+{
+  double Lambda = 1.14;
+
+  return pow(sq(Lambda) - t,-4.) * 3./(pow(sq(Lambda) - tmin,-3.) - pow(sq(Lambda) - tmax,-3.));
+}
--- src/libraries/cross_sections/photoCrossSection.hh
+++ src/libraries/cross_sections/photoCrossSection.hh
@@ -1,8 +1,6 @@
 #ifndef __PHOTO_CROSS_SECTIONS_H__
 #define __PHOTO_CROSS_SECTIONS_H__
 
-#include "TVector3.h"
-
 class photoCrossSection
 {
  public:
@@ -12,6 +10,12 @@ class photoCrossSection
   double sigma_pi0_n(double s, double t);
   double sigma_pip_n(double s, double t);
   double sigma_pim_p(double s, double t);
+  double sigma_psi_p(double s, double t);
+  double sigma_psi_p(double s, double t, double QSq);
+  double R_psi_p(double QSq);
+
+private:
+  double dipole_F(double t, double tmin, double tmax);
   
 };
 
